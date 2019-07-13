6f246b3f416a94a7a196d50b57f015d95556af3b
Alex Meier <ameier6775@gmail.com>
2019-07-13 14:41:47 -0500

    removing unneeded files

--- service/src/main/java/com/example/helloworld/controller/TeamController.java
+++ service/src/main/java/com/example/helloworld/controller/TeamController.java
@@ -1,15 +1,10 @@
 package com.example.helloworld.controller;
 
-import com.example.helloworld.model.HockeyTeam;
 import com.example.helloworld.services.HockeyTeamService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
